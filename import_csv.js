c79de13567159a98247f384dece7d1e9592dcb3a
Will Clapp <wsclapp@stanford.edu>
2020-05-12 18:25:32 -0500

    prepared array for asset preloading

--- import_csv.js
+++ import_csv.js
@@ -3,31 +3,58 @@ const csv = require('csv-parser')
 let exposureResults = []
 let trialResults = []
 
-fs.createReadStream('experiment_no_shift/trial_csv/exposure.csv')
-	.pipe(csv())
-	.on('data', data => exposureResults.push(data))
-	.on('end', () => {
-		console.log('done reading exposure.csv')
-		fs.createReadStream('experiment_no_shift/trial_csv/test.csv')
-			.pipe(csv())
-			.on('data', data => trialResults.push(data))
-			.on('end', () => {
-				console.log('done reading test.csv')
-				writeResults(exposureResults, trialResults)
-			})
-	})
+let assetPaths = []
+
+const buildOutput = (exposure, trial, assets) => `
+var exposure_stimuli = ${JSON.stringify(exposure, null, 2)}
+
+var trial_stimuli = ${JSON.stringify(trial, null, 2)}
+
+var assetPaths = ${JSON.stringify(assets.flat(), null, 2)}`
 
 
-
-
-const buildOutput = (exposure, trial) => `var exposure_stimuli = ${JSON.stringify(exposure, null, 2)}\n\nvar trial_stimuli = ${JSON.stringify(trial, null, 2)}`
-
-
-const writeResults = (exposure, trial) => {
-	fs.writeFile('experiment_no_shift/js/stims.js', buildOutput(exposure, trial), err => {
+const writeResults = (exposure, trial, assets) => {
+	fs.writeFile('experiment_no_shift/js/stims.js', buildOutput(exposure, trial, assets), err => {
 		if (err) {
 			console.error(err)
 		}
 		console.log("stims.js written")
 	})
 }
+
+fs.createReadStream('experiment_no_shift/trial_csv/exposure.csv')
+	.pipe(csv())
+	.on('data', data => {
+		exposureResults.push(data)
+		assetPaths.push([
+			'audio/continua/' + data.stim_audio, 
+			'audio/context/' + data.context_audio, 
+			'audio/continuation/' + data.continuation_audio, 
+			'images/' + data.comp_image, 
+			'images/' + data.target_image
+		])
+	})
+	.on('end', () => {
+		console.log('done reading exposure.csv')
+		fs.createReadStream('experiment_no_shift/trial_csv/test.csv')
+			.pipe(csv())
+			.on('data', data => {
+				trialResults.push(data)
+				assetPaths.push([
+					'audio/continua/' + data.stim_audio, 
+					'images/' + data.comp_image, 
+					'images/' + data.target_image
+				])
+			})
+			.on('end', () => {
+				console.log('done reading test.csv')
+				writeResults(exposureResults, trialResults, assetPaths)
+			})
+	})
+
+
+
+
+
+
+
