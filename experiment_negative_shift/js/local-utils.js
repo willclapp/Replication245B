4defcaf50c29c4f2fdefa750e5a5a47a9d60ffb3
Will Clapp <wsclapp@stanford.edu>
2020-05-18 02:03:19 -0500

    added negative shift condition. Still need to work out kinks tho

---
+++ experiment_negative_shift/js/local-utils.js
@@ -0,0 +1,23 @@
+function build_trial_option(img_path, type) {
+	var trial_option = $("<div>")
+		.addClass("trial_option")
+
+	var image_container = $("<div>")
+		.addClass(type + "_image")
+
+	var radio_button = $("<input />")
+		.attr("type", "radio")
+		.attr("name", "selection")
+		.attr("value", type)
+
+	var image = $("<img />")
+		.attr("src", "images/" + img_path)
+
+	image_container.append(image)
+	trial_option
+		.append(image_container)
+		.append(radio_button)
+
+	return trial_option
+}
+
